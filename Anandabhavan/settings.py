39a6d56bf8c8b4a543972fe463a592ac4d11efb1
Rohan7072 <rohnaraikar007@gmail.com>
2024-03-19 23:40:08 +0530

    Anandabhavan

--- Anandabhavan/settings.py
+++ Anandabhavan/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ananadabhavan',
         'HOST': 'localhost',
-        'PORT': '3306',
+        'PORT': '3307',
         'USER': 'root',
         'PASSWORD': '',
 
