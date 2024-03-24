a6b77c438585c4a8a40569f668a015cd0caa8b0f
Suraj Giri <surajgiri974@gmail.com>
2024-03-24 15:50:08 +0530

    Anandabhavan

--- customer/views.py
+++ customer/views.py
@@ -27,15 +27,21 @@ class customer_functionality:
         try:
             email = request.POST.get('email',False)
             password = request.POST.get('password',False)
-            if Customer.objects.get(customer_email = email, customer_password = password):
-                request.session['customer_email'] = email
-                return HttpResponseRedirect('/property_list')
-            else:
-                messages.add_message(request,messages.ERROR,'Invalid Credential')
-                return HttpResponseRedirect('/')
+            customer = Customer.objects.get(customer_email = email)
+            if customer.customer_email == email:
+                if customer.customer_password == password:
+                    request.session['customer_email'] = email
+                    return HttpResponseRedirect('/property_list')
+                else:
+                    messages.add_message(request,messages.ERROR,'Invalid Credential')
+                    return HttpResponseRedirect('/')
         except Customer.DoesNotExist:
             messages.add_message(request,messages.ERROR,'User Does Not Exist')
             return HttpResponseRedirect('/')
+        except:
+            messages.add_message(request,messages.ERROR,'ERROR')
+            return HttpResponseRedirect('/')
+            
             
     def logout(request):
         try:
@@ -47,17 +53,18 @@ class customer_functionality:
             return HttpResponseRedirect('/')
     
     def sign_up(request):
-        
-        customer_name = request.POST.get('customer_name', False)
-        customer_email = request.POST.get('customer_email',False)
-        customer_phone = request.POST.get('customer_phone',False)
-        password = request.POST.get('password',False)
-        customer = Customer(
-            customer_name = customer_name,
-            customer_email = customer_email,
-            customer_mobile = customer_phone,
-            customer_password = password,
-            account_type = 1)
-        customer.save()
-        return HttpResponseRedirect('/')        
-            
+        try:
+            customer_name = request.POST.get('customer_name', False)
+            customer_email = request.POST.get('customer_email',False)
+            customer_phone = request.POST.get('customer_phone',False)
+            password = request.POST.get('password',False)
+            customer = Customer(
+                customer_name = customer_name,
+                customer_email = customer_email,
+                customer_mobile = customer_phone,
+                customer_password = password,
+                account_type = 1)
+            customer.save()
+            return HttpResponseRedirect('/')        
+        except:
+            return HttpResponseRedirect('/')                     
